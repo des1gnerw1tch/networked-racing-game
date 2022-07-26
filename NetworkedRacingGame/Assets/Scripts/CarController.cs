6292c26b52cbe3184fff7100693993a498c128af
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2022-07-26 15:00:12 -0400

    added new steering settings for each vehicle

--- NetworkedRacingGame/Assets/Scripts/CarController.cs
+++ NetworkedRacingGame/Assets/Scripts/CarController.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+	
 	[SerializeField] private WheelCollider flw;
 	[SerializeField] private WheelCollider frw;
 	[SerializeField] private WheelCollider blw;
 	[SerializeField] private WheelCollider brw;
+	
     [SerializeField] private float acceleration = 20;
     [SerializeField] private float maximumVelocity = 100;
-
+	[SerializeField] private float turnSpeed = 0.05f;
+	[SerializeField] private float maxTurn = 2.0f;
+	
     private bool gasPressed;
 	private bool leftPressed;
 	private bool rightPressed;
 	private float steerFactor = 0;
-	
-	
      
 	private void Start()	{
 		rb.centerOfMass = new Vector3(0.0f, -0.5f, -0.0f);
@@ -26,9 +28,9 @@ public class CarController : MonoBehaviour
 		}
 	}
 	
-    private void OnGas() {gasPressed = true; rb.drag = 0;}
+    private void OnGas() {gasPressed = true; }//rb.drag = 1;}
 
-    private void OnGasReleased() {gasPressed = false; rb.drag = 1;}
+    private void OnGasReleased() {gasPressed = false; }//rb.drag = 1;}
 	
     private void OnLeft() {leftPressed = true;}
 	
@@ -65,13 +67,13 @@ public class CarController : MonoBehaviour
             
         }
 		
-		if (rightPressed && steerFactor < 2.0f) {steerFactor += 0.05f;}
-		if (leftPressed && steerFactor > -2.0f) {steerFactor -= 0.05f;}
+		if (rightPressed && steerFactor < maxTurn) {steerFactor += turnSpeed;}
+		if (leftPressed && steerFactor > -maxTurn) {steerFactor -= turnSpeed;}
 		if (!leftPressed && !rightPressed) {steerFactor -= steerFactor/5;}
 		
 		if (!flw.GetGroundHit(out hit) && !frw.GetGroundHit(out hit))
 		{
-			steerFactor -= steerFactor/3;
+			steerFactor -= steerFactor/5;
 			
 		}
 		rb.transform.Rotate(0.0f, steerFactor, 0.0f, Space.Self);
