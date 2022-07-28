d96bc3d32efe5818e580e2e7ac5e77cb36db9e94
des1gnerw1tch <zachcanon@gmail.com>
2022-07-27 23:38:34 -0400

    Fixed start position bugs

--- NetworkedRacingGame/Assets/Scripts/RaceLogic/SetVehicleStartPosition.cs
+++ NetworkedRacingGame/Assets/Scripts/RaceLogic/SetVehicleStartPosition.cs
@@ -6,25 +6,25 @@ namespace RaceLogic
     [RequireComponent(typeof(PhotonView))]
     public class SetVehicleStartPosition : MonoBehaviour
     {
-        private PhotonView photonView;
-        
         private void Start()
         {
-            photonView = GetComponent<PhotonView>();
-            photonView.RPC("RPC_RequestVehicleStartingPosition", RpcTarget.MasterClient);
+            if (PhotonView.Get(this).IsMine)
+            {
+                PhotonView.Get(this).RPC("RPC_RequestVehicleStartingPosition", RpcTarget.MasterClient);
+            }
         }
         
         [PunRPC]
         private void RPC_RequestVehicleStartingPosition()
         {
             Transform startPosition = TrackNetworkManager.Instance.GetNextStartingPosition();
-            photonView.RPC("RPC_SetVehicleStartingPosition", RpcTarget.All, startPosition.position, startPosition.rotation);
+            PhotonView.Get(this).RPC("RPC_SetVehicleStartingPosition", RpcTarget.All, startPosition.position, startPosition.rotation);
         }
 
         [PunRPC]
         private void RPC_SetVehicleStartingPosition(Vector3 pos, Quaternion rot)
         {
-            if (photonView.IsMine)
+            if (PhotonView.Get(this).IsMine)
             {
                 transform.SetPositionAndRotation(pos, rot);
             }
